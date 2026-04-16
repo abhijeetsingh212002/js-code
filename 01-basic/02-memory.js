6eafce0cc05445523d7cf681c3a6ac6e60279f64
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-16 05:26:56 +0000

    string proto

---
+++ 01-basic/02-memory.js
@@ -0,0 +1,25 @@
+// 2 types of memory stack , heap
+// stack memory is used in all primitive data types
+
+//heap is used in all Non Primitive types
+
+
+
+// const s= "hellow"
+
+// console.log(s.length)
+
+// const a= new String("worlds")
+
+// console.log(a.length);
+
+// let  a= "HellOw"
+
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+
+
+let a = "  hellow  "
+
+console.log(a)
+console.log(a.trim())
