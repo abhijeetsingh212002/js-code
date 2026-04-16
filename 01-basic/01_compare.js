1150a884f057d7a7b9e0fb1323b8e9130e827f27
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-16 04:55:58 +0000

    datatypes

---
+++ 01-basic/01_compare.js
@@ -0,0 +1,33 @@
+console.log("2">1)
+
+let id = Symbol('abc')
+
+let name = Symbol('abc')
+
+console.log(id=== name)
+
+let a = "abc"
+let b= 100
+let c= true
+let d= null
+let e
+let g= Symbol("ab")
+
+const h=["2bhk","3bhk","4bhk"]
+let i={
+    name:"james",
+    id:223
+};
+let j = function(){
+    console.log("true")
+}
+
+console.log(`string: ${typeof a}`)
+console.log(`number: ${typeof b}`)
+console.log(`boolean: ${typeof c}`)
+console.log(`null: ${typeof d}`)
+console.log(`undefine: ${typeof e}`)
+console.log(`symbol: ${typeof g}`)
+console.log(`array: ${typeof h}`)
+console.log(`object: ${typeof i}`)
+console.log(`function: ${typeof j}`)
