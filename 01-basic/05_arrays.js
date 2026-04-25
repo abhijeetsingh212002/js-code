e6136516a999098e5578c0d2379ee0537453593f
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-25 12:50:10 +0000

    arrays

---
+++ 01-basic/05_arrays.js
@@ -0,0 +1,107 @@
+// console.log("arrays")
+
+// const myarr=[0,1,2,3,4,5]
+
+
+// myarr.push(6)
+// myarr.push(7)
+
+// myarr.pop()
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9))
+
+
+// myarr.forEach((num)=>{
+//     console.log(num)
+
+// });
+// const arr = [1, 2, 3];
+
+// arr.forEach((num) => {
+//   console.log(num);
+// });
+
+
+// const arr= myarr.map(num=>num+10)
+
+// const arr= myarr.filter(num=>num>=2);
+
+// const sum = myarr.reduce((acc,curr)=>acc+curr,0)
+// console.log(sum)
+
+// console.log("a",myarr)
+
+
+// const arr=myarr.slice(1,3)
+
+// console.log(arr)
+// console.log(myarr)
+
+
+// console.log("b",myarr)
+
+// const marr= myarr.splice(1,2)
+
+// console.log(marr)
+
+// console.log(myarr)
+
+
+const marvel = ["thor","spiderman","hulk","ironman"]
+
+const dc=["superman","batman","flash"]
+
+
+    //  merge 2 arrays     
+
+// marvel.push(dc)
+
+// console.log(marvel)
+// console.log(dc)
+
+
+
+// 2nd method 
+
+// const dcmarvel= marvel.concat(dc)
+// console.log(dcmarvel)
+
+
+
+    //  3rd and best method
+
+
+    // const alldcmarvel= [...marvel,...dc]
+
+    // console.log(alldcmarvel)
+
+
+
+// another way
+
+
+// const arr=[1,2,3,[4,5,6],[7,8,[4,5]]]
+
+// console.log(arr)
+// const newarr= arr.flat(Infinity)
+// console.log(newarr)
+
+
+
+
+// console.log(Array.isArray("apple"))
+// console.log(Array.from("apple"))
+
+
+//         important case
+// console.log(Array.from({name:"apple"}))
+
+
+// let score =100
+// let score1 =200
+// let score2 =300
+
+
+// console.log(Array.of(score,score1,score2))
