4a843efd8e63ef12621a7387b41f1c9936b3489c
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-25 11:49:48 +0000

    datetimestring

--- 01-basic/04_datesinjs.js
+++ 01-basic/04_datesinjs.js
@@ -1,7 +1,51 @@
 
-let myDate= new Date()
+// let myDate= new Date()
 
-console.log(myDate)
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toLocaleString())
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let myDate= new Date("01-14-2023")
+
+let myTimestamp = Date.now();
+
+// console.log(myTimestamp);
+// console.log(myDate.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let newdate= new Date()
+
+// console.log(newdate)
+// console.log(newdate.getDay())
+// console.log(newdate.getMonth())
+// console.log(newdate.getFullYear())
+
+
+console.log(newdate.toLocaleString())
+
+let name =newdate.toLocaleString('default',{
+    month:"long"
+})
+
+console.log(name)
+
+console.log(myDate.toLocaleString('default',{
+    day:"numeric",
+    month:"long",
+    year:"numeric"
+}))
+
+console.log(myDate.toLocaleString('default',{
+    day:"2-digit",
+    month:"long",
+    year:"numeric"
+}))
+console.log(myDate.toLocaleString('default',{
+    day:"2-digit",
+    month:"long",
+    year:"numeric",
+    minute:"2-digit",
+    hour:"2-digit"
+}))
