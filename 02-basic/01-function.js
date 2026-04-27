e8155372892868f0ff39f85aa9774fc4c60d605b
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-27 05:46:35 +0000

    function

---
+++ 02-basic/01-function.js
@@ -0,0 +1,19 @@
+
+
+function myname(){
+    console.log("a")
+    console.log("p")
+    console.log("p")
+    console.log("l")
+    console.log("e")
+
+}
+
+// myname()
+
+
+function addnumber(num1,num2){
+    console.log(num1+num2)
+}
+
+addnumber(3,4) 
