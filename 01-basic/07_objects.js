b1cc5da50d200a206416ed55101c14cd2bd7e8be
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-27 05:15:01 +0000

    json

---
+++ 01-basic/07_objects.js
@@ -0,0 +1,78 @@
+// // console.log("hello")
+
+
+// const tinderusers = new Object()
+
+
+// tinderusers.id="123abc"
+// tinderusers.name="saaam"
+// tinderusers.isloggedin=false
+
+// // console.log(tinderusers)
+
+// const regularuser={
+//     email:"abc@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"aaaa",
+//             lastname:"asas"
+
+//         }
+//     }
+// }
+
+// // console.log(regularuser.fullname.userfullname.firstname)
+
+// const obj1 ={
+//     1:"a",2:"b"
+// }
+// const obj2={
+//     3:"a",4:"b"
+// }
+
+// const obj3= {...obj1,...obj2}
+// const obj4= Object.assign({}, obj1,obj2)
+
+// console.log(obj4)
+// console.log(obj3)
+
+
+const shop ={
+    name:"ajaja",
+    price:"999",
+    brand:"hero"
+}
+
+// const {brand:a1} = shop
+// const {brand} = shop
+
+// console.log(brand)
+
+// console.log(a1)
+
+// const navbar=({company})=>{
+
+
+// }
+
+// navbar(company="aaaaaa")
+
+//api
+
+// api in object format
+// {
+//     "name":"aaa",
+//     "cname":"abahd",
+//     "price":"888"
+
+// }
+
+
+
+// api - object inside array format
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
