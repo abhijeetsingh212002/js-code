7f806c96a7e6275d806545e34d745b2ef223b9e8
abhijeetsingh212002 <151195956+abhijeetsingh212002@users.noreply.github.com>
2026-04-29 13:51:49 +0000

    switch

---
+++ 03-control-flow/switch.js
@@ -0,0 +1,28 @@
+
+const month= 5
+
+
+switch(month){
+    case 1:
+        console.log("jan")
+
+    break;
+
+    case 2:
+        console.log("feb")
+
+    break;
+
+    case 3:
+        console.log("mar")
+
+    break;
+    case 4:
+        console.log("apr")
+
+    break;
+
+    default:
+        console.log("default case cehck your input")
+        break;
+}
